66964796c0d474513f506d90f0902939279c35f6
Jochem de Kruijf <jochemdekruijfff@outlook.com>
2019-02-20 13:27:01 +0100

    first commit

---
+++ form.js
@@ -0,0 +1,30 @@
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+} 
+
+var name  = document.forms["fname"];
+var lname  = document.forms["lname"];
+var number  = document.forms["number"];
+var mail  = document.forms["mail"];
+var postcode  = document.forms["postcode"];
+var place  = document.forms["place"];
+var lorem  = document.forms["lorem"];
+var gender  = document.forms["gender"];
+var bday  = document.forms["bday"];
+
+
+
+function submit() {
+
+  
+
+var  = document.forms["fname"] === "" ? y : alert('empty');
+
+
+
+
+}
