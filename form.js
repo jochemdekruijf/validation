c8ce0aa29fb9467b8e04311744fbc2b1650532aa
Jochem de Kruijf <jochemdekruijfff@outlook.com>
2019-03-11 10:04:52 +0100

    js form html form af, js form bezig

--- form.js
+++ form.js
@@ -1,30 +1,35 @@
-function validateForm() {
-  var x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
+// function validateForm() {
+//   var x = document.forms["myForm"]["fname"].value;
+//   if (x == "") {
+//     alert("Name must be filled out");
+//     return false;
+//   }
+// } 
+
+var name  = document.forms["jsForm"]["firstname"];
+var lname  = document.forms["jsForm"]["lastname"];
+var number  = document.forms["jsForm"]["number"];
+var mail  = document.forms["jsForm"]["mail"];
+var postcode  = document.forms["jsForm"]["postcode"];
+var place  = document.forms["jsForm"]["place"];
+var lorem  = document.forms["jsForm"]["lorem"];
+
+
+function validation() {
+
+	console.log(firstname.value);
+console.log(lastname.value);
+console.log(number.value);
+console.log(mail.value);
+console.log(postcode.value)
+console.log(place.value);
+console.log(lorem.value);
+alert('test');
+
+  // if (name.value !== "") {
+  //      console.log('niet leeg');  
+
+
   }
-} 
-
-var name  = document.forms["fname"];
-var lname  = document.forms["lname"];
-var number  = document.forms["number"];
-var mail  = document.forms["mail"];
-var postcode  = document.forms["postcode"];
-var place  = document.forms["place"];
-var lorem  = document.forms["lorem"];
-var gender  = document.forms["gender"];
-var bday  = document.forms["bday"];
 
 
-
-function submit() {
-
-  
-
-var  = document.forms["fname"] === "" ? y : alert('empty');
-
-
-
-
-}
